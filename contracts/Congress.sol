292ff960bbe8848c6ba459e3a16a8218c8bb0ba6
Johan Oakes <johanoakes@gmail.com>
2017-11-26 04:52:32 -0500

    Add template for a proposal-voting, Owner deciding Congress

--- contracts/Congress.sol
+++ contracts/Congress.sol
@@ -1,8 +1,46 @@
 pragma solidity ^0.4.2;
 
-import "./Owned.sol" as Owned;
-import "./TokenRecipient.sol" as TokenRecipient;
+import "./Owned.sol";
+import "./TokenRecipient.sol";
 
-contract Congress {
+contract Congress is Owned, TokenRecipient {
+    
+    // Contract variables
+    uint public minimumQuorum;
+    uint public debatingPeriodInMinutes;
+    int public majorityMargin;
+    Proposal[] public proposals;
+    uint public numProposals;
+    mapping (address => uint) public memberId;
+    Member[] public members;
+
+    // Contract Events
+
+    struct Proposal {
+        address recipient;
+        uint amount;
+        string description;
+        uint votingDeadline;
+        bool executed;
+        bool proposalPassed;
+        uint numberOfVotes;
+        int currentResult;
+        bytes32 proposalHash;
+        Vote[] votes;
+        mapping (address => bool) voted;
+    }
+
+    struct Member {
+        address member;
+        string name;
+        uint memberSince;
+    }
+
+    struct Vote {
+        bool inSupport;
+        address voter;
+        string justification;
+    }
+
 
 }
