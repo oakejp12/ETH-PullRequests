f56eeb2327a2fa24bae34adb309b300ef5efee96
Johan Oakes <johanoakes@gmail.com>
2017-12-10 18:23:36 -0500

    Make sure the structure of issues make sense in terms of functionality and accessbility...and that they work

--- src/js/app.js
+++ src/js/app.js
@@ -4,6 +4,30 @@ App = {
 
     init: function() {
         // Load any necessary data (i.e. git repo related data...)
+        // i.e. load all the current proposals being voted on...
+
+        // Grab issues JSON
+        // TODO: Server exposes path to serve (API)
+        // TODO: Call API - e.g. github_data("/repos", "repos");
+        $.getJSON('/issues', function(data) {
+            let issuesRow = $('#issuesRow');
+            let issueTemplate = $('#issueTemplate');
+
+            const data_arr = data.data;
+            console.log(data_arr);
+
+            data_arr.forEach(element => {
+                console.log(element);
+
+                // Populate template with issues description
+                issueTemplate.find('.panel-title').text(element.title);
+                issueTemplate.find('.issue-description').text(element.body_text);
+                issueTemplate.find('.issue-author').text(element.user.login);
+                issueTemplate.find('.btn-vote').attr('data-id', element.id);
+
+                issuesRow.append(issueTemplate.html())
+            });
+        });
 
         return App.initWeb3();
     },
@@ -13,12 +37,12 @@ App = {
          * Check to see if there's a web3 instance already active
          * Mist or Chrome with MetaMask extension will inject their
          *  own web3 instance
+         * If not, create a web3 object based on local provided.
          */
 
-        if (typeof web3 !== 'undefined') // If there is an injected web3 instance
-            App.web3Provider = web3.currentProvider;
-        else // Create our web3 object based on our local provider
-            App.web3Provider = new Web3.providers.HttpProvider('http://localhost:8545')
+        App.web3Provider = (typeof web3 !== 'undefined') 
+            ? web3.currentProvider 
+            : new Web3.providers.HttpProvider('http://localhost:8545'); 
 
         web3 = new Web3(App.web3Provider)
 
@@ -26,33 +50,35 @@ App = {
     },
 
     initContract: function() {
-        $.getJSON('Congress.json', (data) => {
+        
+        // TODO: Comment out when ready....
+        // $.getJSON('Congress.json', (data) => {
 
-            /**
-             * Artifacts are info about our contract such as its deployed
-             * address and ABI (JS object defining how to interact with the
-             * contract including its variables, function and their params).
-             */
+        //     /**
+        //      * Artifacts are info about our contract such as its deployed
+        //      * address and ABI (JS object defining how to interact with the
+        //      * contract including its variables, function and their params).
+        //      */
 
-            // Get the necessary contract artifact file and instantiate it with truffle-contract
-            var CongressArtifact = data;
+        //     // Get the necessary contract artifact file and instantiate it with truffle-contract
+        //     var CongressArtifact = data;
 
-            /**
-            * truffle-contract keeps information about the contract in sync
-            * with migrations so you don't need to change the contract's
-            * deployed address manually
-            *
-            * Creates an instance of the contract we can interact with
-            */
-            App.contracts.Congress = TruffleContract(CongressArtifact);
+        //     /**
+        //     * truffle-contract keeps information about the contract in sync
+        //     * with migrations so you don't need to change the contract's
+        //     * deployed address manually
+        //     *
+        //     * Creates an instance of the contract we can interact with
+        //     */
+        //     App.contracts.Congress = TruffleContract(CongressArtifact);
 
-            // Set the provider for our contract
-            App.contracts.Congress.setProvider(App.web3Provider)
+        //     // Set the provider for our contract
+        //     App.contracts.Congress.setProvider(App.web3Provider)
 
-            // Use our contract to retrieve and mark the voted proposals
-            // in the case some are already voted on from a previous visit
-            // return App.markVoted();
-        });
+        //     // Use our contract to retrieve and mark the voted proposals
+        //     // in the case some are already voted on from a previous visit
+        //     // return App.markVoted();
+        // });
 
         return App.bindEvents();
     },
@@ -63,25 +89,33 @@ App = {
 
     /**
      * Mark existing proposals with the correct metrics.
+     * 
+     * @param {*} votedMembers
+     * @param {*} account
      */
     markVoted: function(votedMembers, account) {
-        var congressInstance;
+        let congressInstance;
 
         App.contracts.Congress.deployed().then((instance) => {
             congressInstance = instance;
 
-        // Using call allows us to read data from the blockchain without
-        // having to send a full transaction (i.e. no Ether spent)
-        return congressInstance.getVoters().call();
-    }).then((voters) => {
+            // Using call allows us to read data from the blockchain without
+            // having to send a full transaction (i.e. no Ether spent)
+            return congressInstance.getVoters().call();
+        }).then((voters) => {
             for (i = 0; i < voters.length; i++) {
-            if (voters[i] !== '0x0000000000000000000000000000000000000000') {
-                $('.panel-pet').eq(i).find('button').text('Success').attr('disabled', true);
+                if (voters[i] !== '0x0000000000000000000000000000000000000000') {
+                    $('.panel-pet').eq(i).find('button').text('Success').attr('disabled', true);
+                }
             }
-        }
-    }).catch((err) => console.error(err.message));
+        }).catch((err) => console.error(err.message));
     },
 
+    /**
+     * Strike a vote on an existing proposal.
+     * 
+     * @param {*} event
+     */
     handleVote: function(event) {
         event.preventDefault();
 
@@ -90,8 +124,8 @@ App = {
         var congressInstance;
 
         // Use web3 to get the user's accounts...
-        web3.eth.getAccounts((error, accounts) => {
-            if (error) console.error(error.message);
+        web3.eth.getAccounts((err, accounts) => {
+            if (err) console.error(err.message);
 
             var account = accounts[0];
 
@@ -106,7 +140,7 @@ App = {
                 // Execute vote as a transaction by sending account
                 return congressInstance.vote(proposalId, {from: account});
             }).then((result) => {
-            return App.markVoted();
+                return App.markVoted();
             }).catch((err) => console.error(err.message));
         });
     }
