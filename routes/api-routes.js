7f410b4b9721daa2590e6ddb568a29cea3b0116c
Johan Oakes <johanoakes@gmail.com>
2017-12-10 18:22:07 -0500

    Use the current repository as an example until a frontend is built to select a repo

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -4,10 +4,9 @@ module.exports = function(app) {
 
   app.get("/issues", (req, res) => {
     console.log("Hitting the repo path...");
-    request.getAllIssuesForRepo({
-        owner: 'oakejp12', // TODO: Take out hardcoded values!
-        repository: 'ethpull'
-    }, (err, response) => {
+
+    // TODO: TAKE OUT HARDCODED VALUES!
+    request.getAllIssuesForRepo('ethpull', 'oakejp12', (err, response) => {
       if (err) throw new Error("Error hitting Github API: " + err);
       res.json(response);
     });
