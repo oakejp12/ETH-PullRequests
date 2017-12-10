3ff8a03600f8258f2e22b2c478443c467a4633be
Johan Oakes <johanoakes@gmail.com>
2017-12-10 18:59:11 -0500

    Use publicly available repo

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -6,7 +6,7 @@ module.exports = function(app) {
     console.log("Hitting the repo path...");
 
     // TODO: TAKE OUT HARDCODED VALUES!
-    request.getAllIssuesForRepo('ethpull', 'oakejp12', (err, response) => {
+    request.getAllIssuesForRepo('contracts', 'git-token', (err, response) => {
       if (err) throw new Error("Error hitting Github API: " + err);
       res.json(response);
     });
