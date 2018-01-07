e7296f00da3c9deb4cc2d317b61ac200f8871ad5
Johan Oakes <johanoakes@gmail.com>
2018-01-06 20:06:04 -0500

    Use ES6 templating and add comments so I can remember

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -1,14 +1,23 @@
 import request from '../src/js/github_data';
 
+/*
+* Expose API for the client to query
+*/
+
 module.exports = function(app) {
 
+  /**
+   * Retrieve all issues from a repository...
+   */
   app.get("/issues", (req, res) => {
     console.log("Hitting the repo path...");
 
     // TODO: TAKE OUT HARDCODED VALUES!
     request.getAllIssuesForRepo('contracts', 'git-token', (err, response) => {
-      if (err) throw new Error("Error hitting Github API: " + err);
+      if (err) throw new Error(`Error hitting Github API: ${err}`);
       res.json(response);
     });
   });
+
+
 };  
