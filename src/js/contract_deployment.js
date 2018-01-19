ab1cdbb9b7f913d58370f568efad419a207a3fda
Johan Oakes <johanoakes@gmail.com>
2018-01-18 22:22:22 -0500

    Separate contract deployment into separate parts for modularity and define an asynchronous way of handling reads

--- src/js/contract_deployment.js
+++ src/js/contract_deployment.js
@@ -1,13 +1,15 @@
 import redis    from 'redis';
 import contract from 'truffle-contract';
 import Web3     from 'web3';
-import path     from 'path'
+import path     from 'path';
 import fs, { readFileSync } from 'fs';
 
 const REDIS_PORT = process.env.REDIS_PORT || 6379;
 const client = redis.createClient(REDIS_PORT);
 
-const web3Provider = (typeof web3Provider !== 'undefined') ? web3Provider : new Web3.providers.HttpProvider('http://localhost:7545');
+// TODO: In the future, we want to set these to read from truffle.js
+const HOST = 'localhost';
+const PORT = '7545';
 
 /**
  * Retrieve deployed contract address on ethereum network based on the
@@ -17,64 +19,98 @@ const web3Provider = (typeof web3Provider !== 'undefined') ? web3Provider : new 
  * @param {*} owner 
  * @param {*} callback 
  */
-let cache = (req, res, next) => {
+function cache(req, res, next) {
     const org = req.query.organization;
+
+    console.log(`Retrieving contract for ${org}.`);
 
     client.get(org, (err, data) => {
         if (err) throw new Error(`Error retrieving contract address: ${err}`);
 
         if (data != null) {
+            console.log('Contract data cached in Redis: \n' + data);
             res.send(data);
         } else {
+            console.log(`Contract not found for ${org}`);
             next();
         }
     });
 }
 
 /**
- * Deploy a new contract Congress with the proper details
+ * Deploy a new contract Congress with the proper details from a request.
  * 
- * @param {*} req 
- * @param {*} res 
- * @param {*} next
- * @return {*} address - The deployed contract address
+ * The proper details from the request should contain these values:
+ *  req.query.org               --->    Organization (i.e. repository...)
+ *  req.query.quorum            --->    Minimum number of people to participate
+ *  req.query.debatingPeriod    --->    Debating period in Minutes
+ *  req.query.majorityMargin    --->    Margin to add to majority (i.e. 50% + majorityMargin)
+ *    
+ * @param {*} req       - The request object that contrains the 
+ * @param {*} res       - Send back any response...
+ * @param {*} next      - NEXT! 
+ * @return {*} address  - The deployed contract address
  */
-let setContractDetails = (req, res, next) => {
-    console.log("Setting dynamic contract details...");
-    
+function setContractDetails(req, res, next) {
+
     // Retrieve details needed to deploy a new Congress contract
+    console.log('Request details: ');
     console.dir(req.query, { depth: null });
-    const org                       = req.query.organization;
-    const minimumQuorum             = req.query.minimumQuorum;
-    const debatingPeriodInMinutes   = req.query.debatingPeriodInMinutes;
-    const majorityMargin            = req.query.majorityMargin;
     
     // Retreive the compiled contract to extract the ABI and bytecode so a new instance can be deployed
     const contractPath = path.join(__dirname, '../../build/contracts/Congress.json');
-    const sourceCode = JSON.parse(fs.readFileSync(contractPath));
 
-    // TODO: Look into await/async method since this is return undefined at the moment...
-    // const contents = fs.readFileSync('./build/contracts/Congress.json', (err, data) => {
-    //     if (err) throw new Error(`Unable to read smart contract abi: ${err}`);
-    //     return JSON.parse(data);
-    // });
+    fs.readFile(contractPath, (err, data) => {
+        if (err) throw new Error(`Unable to read smart contract file: ${err}`);
+        
+        const contents = JSON.parse(data);
+
+        deployContract(contents.abi, contents.bytecode, req.query);
+    });
+}
+
+/**
+ * Deploy a new contract using the request details.
+ * 
+ * @param {*} abi 
+ * @param {*} bytecode 
+ * @param {*} options
+ */
+function deployContract(abi, bytecode, options) {
+    console.log("Setting dynamic contract details...");
 
     const Congress = contract({
-        abi: sourceCode.abi,
-        unlinked_binary: sourceCode.bytecode
+        abi: abi,
+        unlinked_binary: bytecode
     });
     
-    Congress.setProvider(web3Provider);
+    let web3 = (typeof web3.currentProvider !== 'undefined') ? 
+        new Web3(web3.currentProvider) : 
+        new Web3(new Web3.providers.HttpProvider(`http:\/\/${HOST}:${PORT}`));
+
+    Congress.setProvider(web3.currentProvider);
+
+    // NOTE: Dirty hack - see https://github.com/trufflesuite/truffle-contract/issues/56#issuecomment-331084530
+    if (typeof Congress.currentProvider.sendAsync !== "function") {
+        Congress.currentProvider.sendAsync = () => {
+            return Congress.currentProvider.send.apply(Congress.currentProvider, arguments);
+        }
+    }
 
     // Deploy a new smart contract with requested details...
-    Congress.new(minimumQuorum, debatingPeriodInMinutes, majorityMargin).then((instance) => {
+    // TODO: Make sure options isn't null and that these options are present
+    Congress.new(options.quorum, options.debatingPeriod, options.majorityMargin).then((instance) => {
         const address = instance.address;
         console.log(`Address of the newly deployed contract: ${address}.`)
         return address;
     }).then((data) => {
         // Save contract address to Redis
+        console.log('Saving data to Redis: \n' + data);
+
         // TODO: Should we cache the other contract values as well?
-        client.set(org, address);
+        // client.set(options.org, address);
+    }).catch((err) => {
+        console.error('Error with deploying new smart contract: ' + err);
     });
 }
 
