f19d0ef26b0089930be7255d1475414d5fcb87cb
Johan Oakes <johanoakes@gmail.com>
2018-01-25 19:28:41 -0500

    Add initail React boilerplate

---
+++ src/routes/api-routes.js
@@ -0,0 +1,28 @@
+import { getAllIssuesForRepo }  from '../src/js/github_data';
+import { cache, setContractDetails } from '../src/js/contract_deployment';
+
+/*
+* Expose API for the client to query
+*/
+
+module.exports = function(app) {
+
+  /**
+   * Retrieve all issues from a repository...
+   */
+  app.get("/issues", (req, res) => {
+    console.log("Hitting the repo path...");
+
+    // TODO: TAKE OUT HARDCODED VALUES!
+    getAllIssuesForRepo('contracts', 'git-token', (err, response) => {
+      if (err) throw new Error(`Error hitting Github API: ${err}`);
+      res.json(response);
+    });
+  });
+
+  /**
+   * Retrieve the cached smart contract
+   */
+  app.get("/contract", cache, setContractDetails);
+
+}; 
