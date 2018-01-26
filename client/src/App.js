63bef6acde15a9fa29d3b8fd56e8e0e844a94550
Johan Oakes <johanoakes@gmail.com>
2018-01-25 20:24:01 -0500

    Integrate client code with development server

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,26 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+
+  state = {
+    response: ''
+  };
+
+  componentDidMount() {
+    this.callApi()
+      .then(res => this.setState({ response: res.express }))
+      .catch(err => console.error(err));
+  }
+
+  callApi = async () => {
+    const res = await fetch('/api/hello');
+    const body = await res.json();
+
+    if (res.status !== 200) throw new Error(body.message);
+    
+    return body;
+  };
+
   render() {
     return (
       <div className="App">
@@ -11,7 +31,7 @@ class App extends Component {
           <h1 className="App-title">Welcome to React</h1>
         </header>
         <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+          {this.state.response}
         </p>
       </div>
     );
